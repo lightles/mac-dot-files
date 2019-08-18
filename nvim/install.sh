7e30c7085d54d7f1844ce5617eb738b4cf40a99c
steven.lightle-ca <steven.lightle@wwt.com>
2019-08-18 14:00:22 -0500

    fix nvim install

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,4 +3,5 @@ virtualenv -p python3 $HOME/.config/nvim/pynvim;
 source $HOME/.config/nvim/pynvim/bin/activate;
 pip install pyls pynvim;
 mkdir $HOME/.config/nvim/swapfiles;
+mkdir $HOME/.config/nvim/bundle;
 nvim +'call myCore#UpdatePlugins()' +qall;
